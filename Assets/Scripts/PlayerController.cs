64f43f653aa8136153ebe1969543085787b1b072
Cemre <110540754+Rainy-Mask@users.noreply.github.com>
2023-09-02 15:53:53 +0300

    Mathf function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { 
   Rigidbody physics;
   public int speed;
+  public float xMin, xMax, zMin, zMax;
 
   void Start()
   {
@@ -21,5 +23,12 @@ public class PlayerController : MonoBehaviour
     Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
     physics.velocity = movement * speed;
+
+    Vector3 position = new Vector3(
+      Mathf.Clamp(physics.position.x, xMin, xMax),
+      0,
+      Mathf.Clamp(physics.position.z, zMin, zMax)
+      );
+    physics.position = position;
   }
 }
