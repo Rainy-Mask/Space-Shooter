8b06a60e69889d3c970c68412e6636c0ea37bb22
Cemre <110540754+Rainy-Mask@users.noreply.github.com>
2023-09-04 00:32:32 +0300

    Explosions-astroid moves

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Boundary")
         {
             return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
