5a5da42a37fafd4b110fb7e0c28cd14e9f77cd42
Cemre <110540754+Rainy-Mask@users.noreply.github.com>
2023-09-04 01:10:46 +0300

    Wave

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,22 +8,27 @@ public class GameController : MonoBehaviour
     public int spawnCount;
     public float spawnWait;
     public float startSpawn;
+    public float waveWait;
 
     IEnumerator SpawnValues()
     {
         yield return new WaitForSeconds(startSpawn);
-        for (int i = 0; i < spawnCount; i++)
+        while (true) //sürekli bu kodu çalıştır
         {
-            Vector3 spawnPosition = new Vector3(Random.Range(-3,3),0,10);
-            Quaternion spawnRotation = Quaternion.identity;
-            Instantiate(hazard, spawnPosition, spawnRotation);
-            //Coroutine-kodu bekletmek için
-            //1.IEnumerator döndürmek zorundadır.
-            //2.En az 1 adet yield ifadesi bulundurmak zorundadır.
-            //3.Coroutinler çağrılırken mutlaka StartCoroutine metoduyla çağırılmalıdır.
-            yield return new WaitForSeconds(spawnWait);
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-3,3),0,10);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                //Coroutine-kodu bekletmek için
+                //1.IEnumerator döndürmek zorundadır.
+                //2.En az 1 adet yield ifadesi bulundurmak zorundadır.
+                //3.Coroutinler çağrılırken mutlaka StartCoroutine metoduyla çağırılmalıdır.
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
         }
-      
     }
     void Start()
     {
